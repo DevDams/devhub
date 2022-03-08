ee743d5a45fcd46d17eaa2f39e147a0579eca0a7
DevDams <ad.aimedesire@gmail.com>
2022-03-08 16:55:37 +0000

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="mt-24 pt-3 text-center text-4xl font-bold text-violet-600">
+        Welcome to DevHub
+      </p>
     </div>
   );
 }
