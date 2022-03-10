0b40fce2ea629a23d033e066d66fa8bfa11e7145
DevDams <ad.aimedesire@gmail.com>
2022-03-10 01:11:32 +0000

    optimize design

--- src/components/AppCard/index.js
+++ src/components/AppCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 // components
 import Toggle from "../ThemeToggle/index";
 import ProfilCard from "../ProfilCard/index";
@@ -6,7 +6,11 @@ import ProfilCard from "../ProfilCard/index";
 import SearchIcon from "../../assets/icons/search.svg";
 import SearchIconWhite from "../../assets/icons/search-white.svg";
 
+import { ThemeContext } from "../../context/ThemeContext";
+
 const AppCard = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <>
       <div className="mx-auto mt-10 w-10/12 rounded-xl md:w-3/5 lg:w-3/5 lg:max-w-2xl">
@@ -23,20 +27,20 @@ const AppCard = () => {
         <div className="search-bar mt-2 flex h-12 sm:h-14 w-full items-center justify-between rounded-xl bg-gray-200 shadow-sm">
           <div className="relative flex h-full w-full items-center dark:bg-myblack-dark">
             <img
-              src={SearchIcon}
+              src={ theme === 'light' ? SearchIcon : SearchIconWhite}
               alt="search-icon"
               className="absolute ml-5 hidden w-7 sm:block"
             />
             <input
               type="text"
-              className="h-full w-full rounded-xl border-2 border-gray-100 px-6 text-lg font-medium outline-none focus:border-myblack-gray focus:shadow-sm dark:text-myblack-gray dark:bg-myblack-medium dark:border-transparent dark:focus:border-myblack-gray sm:pl-14"
+              className="h-full w-full rounded-xl border-2 border-gray-100 px-6 text-lg font-medium outline-none focus:border-gray-200 focus:shadow-sm dark:text-myblack-gray dark:bg-myblack-medium dark:border-transparent dark:focus:border-myblack-gray sm:pl-14"
             />
-            <button className="text-md absolute right-0 flex h-full w-1/5 cursor-pointer items-center justify-center rounded-xl bg-violet-500 font-semibold text-white">
+            <button className="text-md absolute right-1 sm:right-2 flex h-10 w-14 sm:w-1/5 cursor-pointer items-center justify-center rounded-xl bg-violet-500 font-semibold text-white">
               <span className="hidden sm:block">Rechercher</span>
               <img
                 src={SearchIconWhite}
                 alt="search-icon"
-                className="block w-7 sm:hidden"
+                className="block w-5 sm:w-7 sm:hidden"
               />
             </button>
           </div>
--- src/components/ProfilCard/index.js
+++ src/components/ProfilCard/index.js
@@ -1,47 +1,79 @@
-import React from "react";
+import React, { useContext } from "react";
+// images and icons
+import PositionIcon from "../../assets/icons/compass.svg";
+import TwitterIcon from "../../assets/icons/twitter.svg";
+import LinkedIcon from "../../assets/icons/linkedin.svg";
+import PositionIconWhite from "../../assets/icons/compass-white.svg";
+import TwitterIconWhite from "../../assets/icons/twitter-white.svg";
+import LinkedIconWhite from "../../assets/icons/linkedin-white.svg";
+
+import { ThemeContext } from "../../context/ThemeContext";
 
 const ProfilCard = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div>
-      <div className="mt-5 shadow-lg flex w-full flex-col items-center rounded-lg border-2 border-gray-100 bg-white py-6 px-6 shadow-sm dark:border-none dark:bg-myblack-medium dark:text-gray-900 sm:flex sm:flex-row sm:items-start lg:px-8">
+      <div className="mt-5 flex w-full flex-col items-center rounded-lg border-2 border-gray-200 bg-white py-6 px-6 shadow-lg shadow-sm dark:border-none dark:bg-myblack-medium dark:text-gray-900 sm:flex sm:flex-row sm:items-start lg:px-8">
         {/* Porfil Picture */}
         <div>
           <div className="h-24 w-24 rounded-full bg-violet-500 sm:h-20 sm:w-20 lg:h-24 lg:w-24"></div>
         </div>
         {/* Profil detail */}
-        <div className="sm:ml-5 mt-8 w-full sm:mt-0">
-          <div className="flex flex-col items-center sm:flex sm:flex-row sm:items-start justify-between">
+        <div className="mt-8 w-full sm:ml-5 sm:mt-0">
+          <div className="flex flex-col items-center justify-between sm:flex sm:flex-row sm:items-start">
             <div className="text-center sm:text-left">
-              <h3 className="text-xl font-bold lg:text-2xl dark:text-myblack-gray">Bob l'éponge</h3>
+              <h3 className="text-xl font-bold dark:text-myblack-gray lg:text-2xl">
+                Bob l'éponge
+              </h3>
               <span className="pt-2 font-semibold text-violet-500">
                 bobdev9_
               </span>
             </div>
             <div>
-              <p className="lg:text-md mt-1 font-medium text-myblack-medium dark:text-myblack-gray">
+              <p className="lg:text-md mt-1 font-medium text-myblack-light dark:text-myblack-gray">
                 Inscrit le 25 janvier 2020
               </p>
             </div>
           </div>
           <div className="mt-5 sm:mt-3">
-            <p className="font-medium text-myblack-medium text-center sm:text-left dark:text-white">
+            <p className="text-center font-medium text-myblack-light dark:text-white sm:text-left">
               ActiveLock continuously authenticates users by the way they type
               to ensure only authorized users are accessing company equipment
             </p>
           </div>
-          <div className="mt-8 flex justify-around rounded-lg bg-myblack-gray dark:bg-myblack-dark px-3 py-1">
-            <div className="text-myblack-dark font-medium flex flex-col items-center">
+          <div className="mt-8 flex justify-around rounded-lg bg-gray-200 px-3 py-1 dark:bg-myblack-dark">
+            <div className="flex flex-col items-center font-medium text-myblack-dark">
               <p className="font-bold dark:text-myblack-gray">Repo</p>
-              <p className="text-white">9</p>
+              <p className="text-myblack-medium dark:text-myblack-gray">9</p>
             </div>
-            <div className="text-myblack-dark font-medium flex flex-col items-center">
+            <div className="flex flex-col items-center font-medium text-myblack-dark">
               <p className="font-bold dark:text-myblack-gray">Followers</p>
-              <p className="text-white">3938</p>
+              <p className="text-myblack-medium dark:text-myblack-gray">3938</p>
             </div>
-            <div className="text-myblack-dark font-medium flex flex-col items-center">
+            <div className="flex flex-col items-center font-medium text-myblack-dark">
               <p className="font-bold dark:text-myblack-gray">Following</p>
-              <p className="text-white">123</p>
+              <p className="text-myblack-medium dark:text-myblack-gray">123</p>
             </div>
+          </div>
+          <div className="mt-8 flex items-center justify-around font-medium text-myblack-dark">
+            <p className="flex items-center">
+              <img src={ theme === 'light' ? PositionIcon : PositionIconWhite} alt="search-icon" className="mr-2 w-5" />
+              <span className="dark:text-myblack-gray text-sm sm:text-md">San Francisco</span>
+            </p>
+            <p className="flex items-center">
+              <img src={ theme === 'light' ?  TwitterIcon : TwitterIconWhite } alt="search-icon" className="mr-2 w-5" />
+              <span className="dark:text-myblack-gray text-sm sm:text-md">Not available</span>
+            </p>
+          </div>
+          <div className="flex items-center justify-around mt-2 font-medium text-myblack-dark">
+            <p className="flex items-center">
+            <img src={ theme === 'light' ?  LinkedIcon : LinkedIconWhite} alt="search-icon" className="mr-2 w-5" />
+              <span className="dark:text-myblack-gray text-sm sm:text-md">San Francisco</span>
+            </p>
+            <p className="flex items-center">
+              <span className="dark:text-myblack-gray text-sm sm:text-md">Not available</span>
+            </p>
           </div>
         </div>
       </div>
